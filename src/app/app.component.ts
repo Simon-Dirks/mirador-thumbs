a55d351d6d5c1c7f0d5ee0245d726c66d915f6de
Simon Dirks <mail@simondirks.com>
2025-04-10 15:25:54 +0200

    Init Mirador with sample manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+// @ts-ignore
+import Mirador from 'mirador/dist/es/src/index';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mirador-thumbs';
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(async () => {
+      const manifestUrl =
+        'https://iiif.io/api/cookbook/recipe/0001-mvm-image/manifest.json';
+
+      const config = {
+        id: 'mirador',
+        windows: [
+          {
+            manifestId: manifestUrl,
+          },
+        ],
+      };
+      const miradorInstance = Mirador.viewer(config);
+    });
+  }
 }
